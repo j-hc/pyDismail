ac8089fe91bae24738ecee27f9f62605771c0fce
scrubjay55 <karayay12@gmail.com>
2021-08-02 23:52:09 +0300

    ready for pypi

--- setup.py
+++ setup.py
@@ -3,10 +3,14 @@ from setuptools import setup
 setup(
     name='pyDismail',
     version='2',
+    project_urls={"Source": "https://github.com/scrubjay55/pyDismail.git"},
+    url="https://github.com/scrubjay55/pyDismail.git",
     author='scrubjay55',
+    license="APLv2",
     packages=['pyDismail'],
     description='an API wrapper for yadim.dismail.de, a disposable mail provider',
     long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     install_requires=[
         "requests",
         "eml-parser"
